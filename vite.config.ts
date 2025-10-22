c0470257ba65c4b7be00ddd76ee9715b1caaf530
unknown <abhinav.bharti@goml.io>
2025-10-22 15:48:39 +0530

    updated host

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "hackathon-alb-535451099.ap-south-1.elb.amazonaws.com",
     port: 8081,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
